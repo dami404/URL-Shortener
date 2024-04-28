9470eb7d7ad8bac2d07ac5d57031e4d4393f1eb9
dami2004 <d.emelianoff@gmail.com>
2024-04-29 01:15:01 +0300

    добавил логгер

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module url-shortener
 go 1.22.2
 
 require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"github.com/common-nighthawk/go-figure"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
 )
 
 func main() {
 
+	figure.NewFigure("URL-Shortener", "", true).Print()
+
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-
-	// TODO: logger: slog
+	// ! сделать свой логгер
+	log := setupLogger(cfg.Env)
+	log.Info("Starting App", slog.String("env", cfg.Env))
+	log.Debug("Debug level is enable")
 
 	// TODO: storage: mongodb
 
 	// TODO: router: chi, chi render
 
 	// TODO: server:
+}
+
+func setupLogger(env string) (log *slog.Logger) {
+	envMap := map[string]*slog.Logger{
+		"local":       slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})),
+		"development": slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})),
+		"production":  slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})),
+	}
+	log = envMap[env]
+	return
 }
